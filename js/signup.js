20b323da336696f15bb4f77df7d0eaaa662c6463
KenichiKai <147292759+KenichiKai@users.noreply.github.com>
2024-03-10 12:07:09 +0700

    last update3

--- js/signup.js
+++ js/signup.js
@@ -3,12 +3,15 @@ import {
     createUserWithEmailAndPassword,
     updateProfile
   } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import Toasty from "./toast.js";
 
   const signUpEmail = document.getElementById("signup-email");
 const signUpUsername = document.getElementById("signup-username");
 const signUpPass = document.getElementById("signup-password");
 const signUpConfirmPassword = document.getElementById("signup-confirm");
 const signUpBtn = document.getElementById("signup-btn");
+const toastHTMLElement = document.getElementById("toast");
+const toastContent = document.getElementById("toast-content");
 
 const auth = getAuth();
 
@@ -19,6 +22,7 @@ signUpBtn.addEventListener("click", (e) => {
     let vietThuong = /[a-z]/g;
     let vietHoa = /[A-Z]/g;
     let chuSo = /[0-9]/g;
+    const signUpToast = new Toasty(toastHTMLElement, toastContent);
 
     if (
         signUpUsername.value.trim().length == 0 ||
@@ -26,17 +30,31 @@ signUpBtn.addEventListener("click", (e) => {
         signUpPass.value.trim().length == 0 ||
         signUpConfirmPassword.value.trim().length == 0
       ) {
-        alert("Something still empty :(");
+        signUpToast.showAlert("Something still empty :(", 'red');
       } else if (signUpPass.value.trim().length < 8) {
-        alert("Password must be at least 8 characters :(");
+        signUpToast.showAlert("Password must be at least 8 characters :(", 'red');
       } else if (!signUpPass.value.trim().match(vietThuong)) {
-        alert("Password must have at least 1 lowercase character :(");
+        signUpToast.showAlert("Password must have at least 1 lowercase character :(", 'red');
       } else if (!signUpPass.value.trim().match(vietHoa)) {
-        alert("Password must have at least 1 uppercase character :(");
+        signUpToast.showAlert("Password must have at least 1 uppercase character :(", 'red');
       } else if (!signUpPass.value.trim().match(chuSo)) {
-        alert("Password must have at least 1 alphanumeric character :(");
+        signUpToast.showAlert("Password must have at least 1 alphanumeric character :(", 'red');
       } else if (signUpPass.value.trim() != signUpConfirmPassword.value.trim()) {
-        alert("Password isn't match :(");
+        signUpToast.showAlert("Password isn't match :(", 'red');
+      } else {
+        createUserWithEmailAndPassword(auth, emailValueSU, passwordValueSU)
+        createUserWithEmailAndPassword(auth, emailValueSU, passwordValueSU)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        window.location.href = "index.html";
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
       }
 });
 
