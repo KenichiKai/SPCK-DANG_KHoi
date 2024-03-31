32bcb35f6e5940afac88c3254762085533bdb95a
KenichiKai <147292759+KenichiKai@users.noreply.github.com>
2024-03-31 11:32:20 +0700

    update

--- js/signup.js
+++ js/signup.js
@@ -1,11 +1,11 @@
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    updateProfile
-  } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 import Toasty from "./toast.js";
 
-  const signUpEmail = document.getElementById("signup-email");
+const signUpEmail = document.getElementById("signup-email");
 const signUpUsername = document.getElementById("signup-username");
 const signUpPass = document.getElementById("signup-password");
 const signUpConfirmPassword = document.getElementById("signup-confirm");
@@ -16,46 +16,46 @@ const toastContent = document.getElementById("toast-content");
 const auth = getAuth();
 
 signUpBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    var emailValueSU = signUpEmail.value;
-    var passwordValueSU = signUpPass.value;
-    let vietThuong = /[a-z]/g;
-    let vietHoa = /[A-Z]/g;
-    let chuSo = /[0-9]/g;
-    const signUpToast = new Toasty(toastHTMLElement, toastContent);
+  e.preventDefault();
+  var emailValueSU = signUpEmail.value;
+  var passwordValueSU = signUpPass.value;
+  let vietThuong = /[a-z]/g;
+  let vietHoa = /[A-Z]/g;
+  let chuSo = /[0-9]/g;
+  const signUpToast = new Toasty(toastHTMLElement, toastContent);
 
-    if (
-        signUpUsername.value.trim().length == 0 ||
-        signUpEmail.value.trim().length == 0 ||
-        signUpPass.value.trim().length == 0 ||
-        signUpConfirmPassword.value.trim().length == 0
-      ) {
-        signUpToast.showAlert("Something still empty :(", 'red');
-      } else if (signUpPass.value.trim().length < 8) {
-        signUpToast.showAlert("Password must be at least 8 characters :(", 'red');
-      } else if (!signUpPass.value.trim().match(vietThuong)) {
-        signUpToast.showAlert("Password must have at least 1 lowercase character :(", 'red');
-      } else if (!signUpPass.value.trim().match(vietHoa)) {
-        signUpToast.showAlert("Password must have at least 1 uppercase character :(", 'red');
-      } else if (!signUpPass.value.trim().match(chuSo)) {
-        signUpToast.showAlert("Password must have at least 1 alphanumeric character :(", 'red');
-      } else if (signUpPass.value.trim() != signUpConfirmPassword.value.trim()) {
-        signUpToast.showAlert("Password isn't match :(", 'red');
-      } else {
-        createUserWithEmailAndPassword(auth, emailValueSU, passwordValueSU)
-        createUserWithEmailAndPassword(auth, emailValueSU, passwordValueSU)
+  if (
+    signUpUsername.value.trim().length == 0 ||
+    signUpEmail.value.trim().length == 0 ||
+    signUpPass.value.trim().length == 0 ||
+    signUpConfirmPassword.value.trim().length == 0
+  ) {
+    signUpToast.showAlert("Something still empty :(", 'red');
+  } else if (signUpPass.value.trim().length < 8) {
+    signUpToast.showAlert("Password must be at least 8 characters :(", 'red');
+  } else if (!signUpPass.value.trim().match(vietThuong)) {
+    signUpToast.showAlert("Password must have at least 1 lowercase character :(", 'red');
+  } else if (!signUpPass.value.trim().match(vietHoa)) {
+    signUpToast.showAlert("Password must have at least 1 uppercase character :(", 'red');
+  } else if (!signUpPass.value.trim().match(chuSo)) {
+    signUpToast.showAlert("Password must have at least 1 alphanumeric character :(", 'red');
+  } else if (signUpPass.value.trim() != signUpConfirmPassword.value.trim()) {
+    signUpToast.showAlert("Password isn't match :(", 'red');
+  } else {
+    createUserWithEmailAndPassword(auth, emailValueSU, passwordValueSU)
+      // createUserWithEmailAndPassword(auth, emailValueSU, passwordValueSU)
       .then((userCredential) => {
         // Signed up
         const user = userCredential.user;
+        signUpToast.showAlert("Sign up successfully !", 'green');
         window.location.href = "signin.html";
-        // ...
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        // ..
+        alert(errorMessage)
       });
-      }
+  }
 });
 
 
